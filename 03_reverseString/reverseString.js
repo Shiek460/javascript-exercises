107a7b692b92d5f7e9f7da5d55a910b957d799e8
Logan Lickfield <l.lickfield@gmail.com>
2024-08-14 14:29:13 -0400

    03-reverseString completed. Declare i at max value, decrement from there. For loop adds each character to a new string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = '';
+    //iterate through string
+    for (let i = string.length-1; i >= 0; i--) {
+        //store each variable in a new string, backwards
+        reversedString += string[i];
+        //start at end, decrement until 0
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
