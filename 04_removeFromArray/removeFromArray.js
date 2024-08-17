eee2073fb49cd74fdc718145d44d7bc4df1e42b8
Logan Lickfield <l.lickfield@gmail.com>
2024-08-16 20:29:21 -0400

    Passed only 6 of 8 tests. Having trouble resolving the last 2: 'removes multiple of the same value' and 'removes multiple values'.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < elements.length; j++) {
+            if (array[i] === elements[j]) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
