085dcb23aa1b98d6c380d2c956aa376b8804f406
Logan Lickfield <l.lickfield@gmail.com>
2024-08-18 12:29:03 -0400

    Completed 05_sumAll. Finished the initial version, then had to toy with  booleans to return expected "ERROR" messages.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num, num2) {
+    let sum = 0;
+    if (num < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (num % 1 != 0 || num2 % 1 != 0 ) {
+        return "ERROR";
+    } else if (typeof(num) !== 'number' || typeof(num2) !== 'number') {
+        return "ERROR"
+    } else if (num < num2) {
+        for (i = num; i <= num2; i++) {
+            sum += i;
+        }
+    } else if (num2 < num) {
+        for (i = num2; i <= num; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
