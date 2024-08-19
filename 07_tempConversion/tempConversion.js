cba455eed156a97bd081a377eb1040d57c9ce50d
Logan Lickfield <l.lickfield@gmail.com>
2024-08-19 16:03:17 -0400

    Completed 07_tempConversions. Found formulas and had to apply a 'toFixed(1)' method to round the decimal place. Also cast back to a number, as 'toFixed' returns a string.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeF) {
+  let celsius = (5/9) * (degreeF - 32);
+  celsius = parseFloat(celsius.toFixed(1));
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeC) {
+  let far = (degreeC * 1.8) + 32;
+  far = parseFloat(far.toFixed(1));
+  return far;
 };
 
 // Do not edit below this line
