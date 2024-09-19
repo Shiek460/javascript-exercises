65812e26d52c5a15d8c54560e05382e243ccad08
Logan Lickfield <l.lickfield@gmail.com>
2024-09-19 14:22:30 -0400

    Completed 10_fibonacci. Populated an array with the first two numbers [1, 1]. Then looped until reaching input to add all subsequent numbers into the sequence. Return array[input - 1].

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let sequence = [1, 1];
+    if (num > 0) {
+        num = parseInt(num);
+        for (let i = 2; i < num; i++) {
+            sequence.push(sequence[i - 2] + sequence[i - 1]);
+        }
+        return sequence[num-1];
+    } else if (parseInt(num) === 0) {
+        return 0;
+    } else if (num < 0) {
+        return "OOPS";
+    }
 };
-
+console.log(fibonacci(4));
+console.log(fibonacci(6));
 // Do not edit below this line
 module.exports = fibonacci;
