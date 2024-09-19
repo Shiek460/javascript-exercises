5b28dca787378816f0d41b691e29e979f2f12a41
Logan Lickfield <l.lickfield@gmail.com>
2024-09-19 13:55:50 -0400

    Completed 09_palindromes. Had more finnicky code than the solution did. But it passed all tests. Scrubbing of the string, followed by reversing, followed by comparison.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    console.log(string);
+    let modified = string
+        .replaceAll(",", "")
+        .replaceAll(" ", "")
+        .replaceAll("!", "")
+        .replaceAll(".", "")
+        .toLowerCase();
+    console.log(modified);
+    let reverse = Array.from(modified);
+    for (let i = 1; i <= reverse.length; i++) {
+        reverse[i - 1] = modified[modified.length - i].toLowerCase();
+    }
+    reverse = reverse
+        .toString()
+        .replaceAll(",", "");
+    console.log(reverse);
+    if (reverse === modified) {
+        return true;
+    } else {
+        return false;
+    }
 };
-
+console.log(palindromes('Racecar!'));
 // Do not edit below this line
 module.exports = palindromes;
