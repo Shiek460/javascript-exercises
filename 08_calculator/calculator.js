826a64a3d2fb05b4368ec7987585d73618336395
Logan Lickfield <l.lickfield@gmail.com>
2024-09-19 11:31:15 -0400

    Completed 08-calculator. Tried to take everything I created and put it into array methods use, to practice. Largely consisted of populating arrays with the proper numbers in a loop, then using 'reduce()'.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, item) => total + item, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, item) => total * item, 1)
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  let array = [];
+  for (let i = 0; i < power; i++) {
+    array.push(number);
+  }
+  return array.reduce((a, b) => a * b, 1);
+  //let current = number;
+  //for (let i = 1; i < power; i++) {
+  //  current *= number;
+  //}
+  //return current;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let array = [];
+  for (let i = num; i > 0; i--) {
+    array.push(i);
+  }
+  return array.reduce((a, b) => a * b, 1);
 };
 
 // Do not edit below this line
