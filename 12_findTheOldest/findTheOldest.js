9ec05e0e0a13b46ce47d903732063b4a2b389fb2
Logan Lickfield <l.lickfield@gmail.com>
2024-09-22 23:56:35 -0400

    Finished 12_findTheOldest. This one got tricky, managing multiple different arrays and making sure the important values were being returned. Ended up doing what I tried to do at first after changing the code order unneccessarily.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    console.log(array);
+    let lifeLengths = 
+        array.map((person) => {
+            if (!person.yearOfDeath) {
+                person.yearOfDeath = (new Date()).getFullYear();
+                return person.yearOfDeath;
+            }
+        array.sort((a, b) => {
+            return (a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth) ? -1 : 1;
+            })
+        return person;
+    });
+    return array[array.length-1];
 };
+
+const people = [
+    {
+        name: "Carly",
+        yearOfBirth: 1066,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+]
+
+findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
